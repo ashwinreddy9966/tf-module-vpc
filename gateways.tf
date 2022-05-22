d0a7fc799fdf9b6b6d79a1c524fc27e7044cea22
manu <manu@local.com>
2022-05-22 19:12:22 +0530

    Auto Push Commit

--- gateways.tf
+++ gateways.tf
@@ -5,3 +5,21 @@ resource "aws_internet_gateway" "igw" {
     Name = "${var.ENV}-IGW"
   }
 }
+
+
+resource "aws_eip" "ngw-eip" {
+  vpc      = true
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw-eip.id
+  subnet_id     = aws_subnet.public.*.id[0]
+
+  tags = {
+    Name = "${var.ENV}-ngw"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
+}
