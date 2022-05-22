e69e9d04ae94f95ab7a09b34f0e2a81e850aac0a
manu <manu@local.com>
2022-05-22 16:19:12 +0530

    Auto Push Commit

--- peering.tf
+++ peering.tf
@@ -2,7 +2,8 @@ resource "aws_vpc_peering_connection" "peer-connection" {
   peer_vpc_id   = var.DEFAULT_VPC_ID
   vpc_id        = aws_vpc.main.id
   auto_accept   = true
-  tags {
+
+  tags = {
     Name = "peer-from-${var.ENV}-to-default-vpc"
   }
 }
