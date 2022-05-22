21a27e0e951606495903a755593bd2cb0ef112af
manu <manu@local.com>
2022-05-22 19:17:06 +0530

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -35,7 +35,7 @@ resource "aws_route_table" "private-route-table" {
 
   route {
     cidr_block                 = "0.0.0.0/0"
-    gateway_id                 = aws_internet_gateway.ngw.id
+    nat_gateway_id                 = aws_nat_gateway.ngw.id
   }
 
   tags = {
