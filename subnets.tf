a85d3e2b9c84b009a47d5df617acced1972f2760
manu <manu@local.com>
2022-05-22 15:52:32 +0530

    Auto Push Commit

--- subnets.tf
+++ subnets.tf
@@ -1,9 +1,10 @@
 resource "aws_subnet" "main" {
-  count      = length(var.SUBNET_CIDR)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.SUBNET_CIDR,count.index )
+  count             = length(var.SUBNET_CIDR)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.SUBNET_CIDR,count.index )
+  availability_zone = element(var.AZ,count.index )
 
   tags = {
-    Name = "Main"
+    Name = "${var.ENV}-${element(var.AZ,count.index )}"
   }
 }
