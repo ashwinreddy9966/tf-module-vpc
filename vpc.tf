84a960e856dbf1e2fe560cfdaec854654a80ec4c
manu <manu@local.com>
2022-05-24 09:12:40 +0530

    Auto Push Commit

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,8 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.VPC_CIDR
-  enable_dns_hostnames = true
-  tags = {
-    Name = "${var.ENV}-vpc"
+  cidr_block             = var.VPC_CIDR
+  enable_dns_hostnames   = true
+  enable_dns_resolutions = true
+      tags = {
+        Name = "${var.ENV}-vpc"
+      }
   }
-}
